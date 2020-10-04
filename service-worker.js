b1596fad43618050e8f0ec552ce8619ee553d210
dodichakill <dodiherlangga91@gmail.com>
2020-10-04 00:24:46 -0700

    masih normal

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,66 @@
-const CACHE_NAME = 'firstpwa-v4';
-var urlsToCache = [
-	'/',
-	'/nav.html',
-	'/index.html',
-	'/pages/home.html',
-	'/pages/TapakSuci.html',
-	'/pages/PagarNusa.html',
-	'/pages/MerpatiPutih.html',
-	'/css/materialize.min.css',
-	'/css/style.css',
-	'/js/materialize.min.js',
-	'/img/as.jpeg',
-	'/img/asad.jpg',
-	'/img/mp.jpg',
-	'/img/perisaidiri.jpg',
-	'/img/perisaiputih.jpg',
-	'/img/pn.jpg',
-	'/img/psht.jpg',
-	'/img/pstd.jpg',
-	'/img/smi.jpg',
-	'/img/ts.jpg',
-    '/js/script.js'
+// const CACHE_NAME = 'firstpwa-v4';
+// var urlsToCache = [
+// 	'/',
+// 	'/nav.html',
+// 	'/index.html',
+// 	'/pages/home.html',
+// 	'/pages/TapakSuci.html',
+// 	'/pages/PagarNusa.html',
+// 	'/pages/MerpatiPutih.html',
+// 	'/css/materialize.min.css',
+// 	'/css/style.css',
+// 	'/js/materialize.min.js',
+// 	'/img/as.jpeg',
+// 	'/img/asad.jpg',
+// 	'/img/mp.jpg',
+// 	'/img/perisaidiri.jpg',
+// 	'/img/perisaiputih.jpg',
+// 	'/img/pn.jpg',
+// 	'/img/psht.jpg',
+// 	'/img/pstd.jpg',
+// 	'/img/smi.jpg',
+// 	'/img/ts.jpg',
+//     '/js/script.js'
     
-];
+// ];
 
-self.addEventListener('install', function(event){
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-	);
-})
+// self.addEventListener('install', function(event){
+// 	event.waitUntil(
+// 		caches.open(CACHE_NAME)
+// 		.then(function(cache) {
+// 			return cache.addAll(urlsToCache);
+// 		})
+// 	);
+// })
 
-self.addEventListener('activate', function(event){
-	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName){
-					if(cacheName != CACHE_NAME){	
-						console.log("ServiceWorker: cache " + cacheName + " telah dihapus");
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-})
+// self.addEventListener('activate', function(event){
+// 	event.waitUntil(
+// 		caches.keys()
+// 		.then(function(cacheNames) {
+// 			return Promise.all(
+// 				cacheNames.map(function(cacheName){
+// 					if(cacheName != CACHE_NAME){	
+// 						console.log("ServiceWorker: cache " + cacheName + " telah dihapus");
+// 						return caches.delete(cacheName);
+// 					}
+// 				})
+// 			);
+// 		})
+// 	);
+// })
 
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request, {cacheName:CACHE_NAME})
-		.then(function(response) {
-			if(response){
-				console.log("ServiceWorker: Menggunakan aset dari cache: ", response.url);
-				return response;
-			}
+// self.addEventListener('fetch', function(event) {
+// 	event.respondWith(
+// 		caches.match(event.request, {cacheName:CACHE_NAME})
+// 		.then(function(response) {
+// 			if(response){
+// 				console.log("ServiceWorker: Menggunakan aset dari cache: ", response.url);
+// 				return response;
+// 			}
 			
-			console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
-			return fetch(event.request);
-		})
-	);
-});
+// 			console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
+// 			return fetch(event.request);
+// 		})
+// 	);
+// });
 
